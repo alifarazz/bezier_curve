6de2a4582920d1e3b707bc27e36874c42480fa38
alifarazz <alifarazdaghi@gmail.com>
2020-01-11 05:37:08 -0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bezier Curves with GPU shaders
+
+A hardware-accelerated implementation of bezier curves, based on Blinn/Loop's method. The method is described in their 2005 SIGGRAPH paper:
+
+https://doi.org/10.1145/1073204.1073303
+
+https://charlesloop.com/LoopBlinn05.pdf
+
+Written in C++17 and OpenGL 3.3.
--- src/include/utils.hh
+++ src/include/utils.hh
@@ -20,18 +20,5 @@ namespace Utils {
         auto operator()(stbi_uc* x) -> void { stbi_image_free(x); }
     };
     using GLFWwindowUniquePtr = std::unique_ptr<GLFWwindow, DestoryGLFWWindowStruct>;
-    using stbi_uc_UniquePtr = std::unique_ptr<stbi_uc*, DestorySTBIImageStruct>;
-    auto loadImageFromFile(const fs::path& path)
-        -> std::tuple<stbi_uc*,
-            int,
-            int,
-            int>
-    {
-        int x, y, comp;
-        stbi_uc* data = stbi_load(path.c_str(), &x, &y, &comp, 0);
-        // std::unique_ptr<stbi_uc *, decltype(free_image)> &&
-        // std::unique_ptr<stbi_uc *, decltype(free_image)>(u, free_image).get()
-        return std::make_tuple(data, x, y, comp);
-    }
 }
 }
